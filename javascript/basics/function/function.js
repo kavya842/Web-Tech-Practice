ca46f1b7e5089881f94ccbce1878bcc33ec6fe27
kavya842 <kavyavankayalapati9999@gmail.com>
2026-03-02 15:13:14 +0530

    update function

--- javascript/basics/function/function.js
+++ javascript/basics/function/function.js
@@ -1,3 +1,4 @@
+//! 1) Named function!//
 //! parametrised function !//
 // function add(y,z){
 //     let x=y+z
@@ -8,13 +9,60 @@
 // add(10,11)
 
 //! parametersied !//
-let a =Number(prompt("enter 1st number"));
-let b =Number(prompt("enter 2nd number"));
-let c =Number(prompt("enter 3rd number"));
-function add(x,y,z)
-{
-    let sum=x+y+z;
-    console.log(sum);
+// let a =Number(prompt("enter 1st number"));
+// let b =Number(prompt("enter 2nd number"));
+// let c =Number(prompt("enter 3rd number"));
+// function add(x,y,z)
+// {
+//     let sum=x+y+z;
+//     console.log(sum);
     
-}
-add(a,b,c)
+// }
+// add(a,b,c)
+
+//! 2)Anonymous function !//
+//! it is a function without a name !//
+//! it is also called lambda function !//
+//! it is used when we want to use a function only once !//
+//! it is used when we want to pass a function as an argument to another function !//
+//! it is used when we want to return a function from another function !//
+//! it is used when we want to create a function that is not accessible from outside the function !//
+// function(){
+//     console.log("I  am an anonymous function")
+// }
+// ()
+
+//! 3) function  with expression !//
+//! when we store a varilabe with a function then it is called function with expression !//
+//! it is also called function expression !//
+// let a=function(a,b){
+//     console.log(a+b)
+//     console.log("I  am an anonymous function")
+// }
+// a(2,3)
+
+//! 4) Immediate invoke function expression (IIFE) !//
+//! when we want to call a function immediately after its declaration then we use IIFE !//
+// (function(){
+//     console.log("I am an IIFE function")
+// })
+// ();
+// (function(a,b){
+//     console.log(a+b)
+// })
+// (2,3)
+//! 5) Arrow function !//
+//! it is a short hand way to write a function !//
+//! it is introduced in ES6 !//
+//! it is also called fat arrow function !//
+//! it does not have its own this keyword !//
+//! it is not suitable for methods !//
+//! it is not suitable for constructors !//
+//! it is not suitable for generator functions !//
+//! it is not suitable for async functions !//
+//! it is not suitable for functions that use arguments object !//
+//! it is not suitable for functions that use super keyword !//
+//! it is not suitable for functions that use new.target keyword !//
+//! it is not suitable for functions that use this keyword !//
+
+
