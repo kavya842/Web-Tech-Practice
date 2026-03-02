78093aa66d7eb68cd20d578118f5dc463bd62f89
kavya842 <kavyavankayalapati9999@gmail.com>
2026-03-02 15:34:32 +0530

    update pop up methods

---
+++ javascript/basics/pop up methods/index.js
@@ -0,0 +1,11 @@
+alert("Welcome to Our Page....");
+console.log("After Login");
+
+let a=Number(prompt("Enter First Number"));
+console.log(typeof a);
+
+// console.log(a+300);
+// console.log("After Prompt!!");
+
+let b=confirm("Are you sure you want to logout?");
+console.log(b);
