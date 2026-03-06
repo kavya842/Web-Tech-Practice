8d0fe4cb778086adbcc78b6c1abb4095d3321678
kavya842 <kavyavankayalapati9999@gmail.com>
2026-03-06 15:47:10 +0530

    update string

--- javascript/basics/string/string.js
+++ javascript/basics/string/string.js
@@ -14,34 +14,68 @@
 
 //! Methods in String !//
 // let str ='JavaScRipT'
+//! lenth()
 // 1) console.log(str.length); // it will give length of the string //
+//! toLowerCase
 // 2)  console.log(str.toLowerCase());
+//! toUpperCase
 // 3) console.log(str.toUpperCase());
+//! at()
 // 4) console.log(str.at(2));
 // 5) console.log(str.at(-2));  // (-ve will work) //
+//! CharAt()
 // 6) console.log(str.charAt(2));
 // 7) console.log(str.charAt(-2)); // (-ve will not work) //
+//! charCodeAt()
 // 8) console.log(str.charCodeAt(1)); // 97 is the output // 
-
+//! replace()
 //9) let str="hello";
 // console.log(str.replace("lo",'p'));
-
+//! replaceAll()
 //10)  let str="hellolololololo";
 // console.log(str.replaceAll("lo",'p'));
-
+//! concat()
 // let str="ja"
 // let str1="va"
 // let str2="script"
 // console.log(str.concat(str1,str2));
+//! trim()
+// let str="          javascript          ";
+// console.log("we are learning " +str + "in web tech");
 
-let str="          javascript          ";
-console.log("we are learning " +str + "in web tech");
+// console.log("we are learning  " +str.trimStart() + " in web tech");
 
-console.log("we are learning  " +str.trimStart() + " in web tech");
+// console.log("we are learning  " +str.trimEnd() + " in web tech");
 
-console.log("we are learning  " +str.trimEnd() + " in web tech");
+// console.log("we are learning  " +str.trim() + "  in web tech");
+//! padstart() , padEnd()
+// let str="java"
+// let str1="script"
+// console.log(str.padStart(8,str1));
+// console.log(str.padStart(12,str1));
 
-console.log("we are learning  " +str.trim() + "  in web tech");
+// console.log(str.padEnd(8,str1));
+// console.log(str.padEnd(12,str1));
+// console.log(str.padEnd(3,str1));
+
+//!indexOf()
+let 
+
+
+
+
+
+
+
+
+
+
+
+
+//! repeat()
+// let str="python "
+// console.log(str.repeat(3));
+
 
 
 
