a1efe3840318227e71967b17622694d2f9c29b58
kavya842 <kavyavankayalapati9999@gmail.com>
2026-03-17 20:01:07 +0530

    update promise

---
+++ javascript/basics/dom/dom.js
@@ -0,0 +1,39 @@
+//! methods in DOM
+//! 1) getElementById()
+let a=document.getElementById("root")
+console.log(root)
+
+//! properties of DOM
+// a.innerHTML="I am root"
+// a.innerHTML="<h3> I am root</h3>"
+
+// a.innerText="i am root"
+// a.innerText="<h3> I am a root</h3>"
+
+// a.textContent="i am root"
+// a.textContent=" i am root"
+
+//! 2)innerText
+// console.log(a.innerText);
+
+//! 3)innerContent
+// console.log(a.innerContent);
+
+//! 4) tagName
+// console.log(a.tagName);
+
+//! 5) id
+// console.log(a.id);
+
+//!6) className
+// a.id="rootElement";
+// a.className="dom_class"
+
+//! 7) style
+// a.style.color="yellow"
+// a.style.backgroundColor="green"
+// a.style.padding="30px"
+// a.style.fontSize="25px"
+
+console.log(a.innerText,"innerText");
+console.log(a.textContent,"textContent");
--- javascript/basics/promise/promise.js
+++ javascript/basics/promise/promise.js
@@ -40,3 +40,170 @@
 // setInterval(()=>{
 //     console.log("Happy Birthday Babulu")
 // },5000)
+
+//! methods in promise
+// !----------all()--------------------------
+// let p1=new Promise((res,rej)=>{
+//     res("I am res p1")
+// })
+// let p2=new Promise((res,rej)=>{
+//     res("I am res p1")
+// })
+// let p3=new Promise((res,rej)=>{
+//     res("I am res p1")
+// })
+
+// Promise.all([p1,p2,p3]).then((v)=>{
+//     console.log(v);
+//     console.log("Then block");
+    
+// }).catch((v)=>{
+//     console.log(v);
+//     console.log("Catch block");
+    
+// }).finally(()=>{
+//     console.log("Finally block");
+    
+// })
+
+// !--output="it is giving output in the form of array of result"-!//
+
+// !---allsettled---------------------!///
+// let p1=new Promise((res,rej)=>{
+//     res("I am res p1")
+// })
+// let p2=new Promise((res,rej)=>{
+//     rej("I am res p1")
+// })
+// let p3=new Promise((res,rej)=>{
+//     res("I am res p1")
+// })
+
+// Promise.allSettled([p1,p2,p3]).then((v)=>{
+//     console.log(v);
+//     console.log("Then block");
+    
+// }).catch((v)=>{
+//     console.log(v);
+//     console.log("Catch block");
+    
+// }).finally(()=>{
+//     console.log("Finally block");
+    
+// })
+
+// !-output--It is giving output in  th form array inside an object is created --it is not possible the execute the catch()----!//
+
+// !-------race()-------!//
+// let p1=new Promise((res,rej)=>{
+//     setTimeout((v)=>{
+//         res("I am res p1")
+//     },30000)
+// })
+// let p2=new Promise((res,rej)=>{
+//     setTimeout(()=>{
+//         res("I am res p2")
+//     },5001)
+// })
+// let p3=new Promise((res,rej)=>{
+//     setTimeout(()=>{
+//         rej("I am res p3")
+//     },5000)
+// })
+
+// Promise.race([p1,p2,p3]).then((v)=>{
+//     console.log(v);
+//     console.log("Then block");
+    
+// }).catch((v)=>{
+//     console.log(v);
+//     console.log("Catch block");
+    
+// }).finally(()=>{
+//     console.log("Finally block");
+    
+// })
+
+// !----it get the result based on the first execution first then it which will executes
+
+// !---------any()--------------
+// let p1=new Promise((res,rej)=>{
+//     res("I am res p1")
+// })
+// let p2=new Promise((res,rej)=>{
+//     res("I am res p1")
+// })
+// let p3=new Promise((res,rej)=>{
+//     res("I am res p1")
+// })
+
+// Promise.any([p1,p2,p3]).then((v)=>{
+//     console.log(v);
+//     console.log("Then block");
+    
+// }).catch((v)=>{
+//     console.log(v);
+//     console.log("Catch block");
+    
+// }).finally(()=>{
+//     console.log("Finally block");
+    
+// })
+
+
+
+
+// let p1=new Promise((res,rej)=>{
+//     setTimeout((v)=>{
+//         res("I am res p1")
+//     },30000)
+// })
+// let p2=new Promise((res,rej)=>{
+//     setTimeout(()=>{
+//         res("I am res p2")
+//     },5001)
+// })
+// let p3=new Promise((res,rej)=>{
+//     setTimeout(()=>{
+//         rej("I am res p3")
+//     },5000)
+// })
+
+// Promise.any([p1,p2,p3]).then((v)=>{
+//     console.log(v);
+//     console.log("Then block");
+    
+// }).catch((v)=>{
+//     console.log(v);
+//     console.log("Catch block");
+    
+// }).finally(()=>{
+//     console.log("Finally block");
+    
+// })
+
+
+// !----------PROMISE Chaining--------------!//
+let p1=new Promise((res,rej)=>{
+    res("i am res p1")
+}).then(()=>{
+    console.log("I am then block");
+    return new Promise((res,rej)=>{
+        rej("i am res in chaining")
+    }).then(()=>{
+        console.log("i am then block in chaining");
+        
+    }).catch(()=>{
+        console.log("I am catch block in chaining");
+        
+    }).finally(()=>{
+        console.log("I am finally block in chaining");
+        
+    })
+}).catch(()=>{
+    console.log("I am catch block.");
+    
+}).finally(()=>{
+    console.log("I am finally !!!");
+    
+})
