7690332cf5ec5ac5062b607b043efb4ea55c6c50
kavya842 <kavyavankayalapati9999@gmail.com>
2026-03-19 15:21:22 +0530

    update dark&whitemodetask

--- javascript/basics/dark&whitemodetask/task.js
+++ javascript/basics/dark&whitemodetask/task.js
@@ -1,16 +1,23 @@
+// let btn=document.getElementById("btn");
+// let mode="dark"
+// btn.addEventListener("click",()=>{
+//     if(mode=="dark"){
+//         mode="light";
+//         document.body.style.backgroundColor="black"
+//         document.body.style.color="white"
+//         btn.textContent="light mode"
+//     }
+//     else{
+//         mode="dark"
+//         document.body.style.backgroundColor="white"
+//         document.body.style.color="black"
+//         btn.textContent="dark mode"
+//     }
+// })
+//! Random colours change when i click the button every time
 let btn=document.getElementById("btn");
-let mode="dark"
 btn.addEventListener("click",()=>{
-    if(mode=="dark"){
-        mode="light";
-        document.body.style.backgroundColor="black"
-        document.body.style.color="white"
-        btn.textContent="light mode"
-    }
-    else{
-        mode="dark"
-        document.body.style.backgroundColor="white"
-        document.body.style.color="black"
-        btn.textContent="dark mode"
-    }
+    let randomColorCode=Math.floor(Math.random()*1000000);
+    document.body.style.backgroundColor=`#${randomColorCode}`;
+    btn.textContent=`colorcode-${randomColorCode}`
 })
