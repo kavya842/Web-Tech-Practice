88c7c47035925f0039086d39d6f96d6bdd4b9e7f
kavya842 <kavyavankayalapati9999@gmail.com>
2026-03-19 14:49:45 +0530

    update dark&whitemodetask

--- javascript/basics/dark&whitemodetask/task.js
+++ javascript/basics/dark&whitemodetask/task.js
@@ -1,4 +1,16 @@
-document.getElementById("btn").addEventListener("click",()=>{
-    
-    alert("I am using the  dark and white toggle theme ")
+let btn=document.getElementById("btn");
+let mode="dark"
+btn.addEventListener("click",()=>{
+    if(mode=="dark"){
+        mode="light";
+        document.body.style.backgroundColor="black"
+        document.body.style.color="white"
+        btn.textContent="light mode"
+    }
+    else{
+        mode="dark"
+        document.body.style.backgroundColor="white"
+        document.body.style.color="black"
+        btn.textContent="dark mode"
+    }
 })
