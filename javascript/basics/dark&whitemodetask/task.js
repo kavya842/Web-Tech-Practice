23d76b21192f02239a442be5d17611de6c60a3c4
kavya842 <kavyavankayalapati9999@gmail.com>
2026-03-19 14:30:17 +0530

    update dark&whitemodetask

---
+++ javascript/basics/dark&whitemodetask/task.js
@@ -0,0 +1,4 @@
+document.getElementById("btn").addEventListener("click",()=>{
+    
+    alert("I am using the  dark and white toggle theme ")
+})
--- javascript/basics/dom/dom2.js
+++ javascript/basics/dom/dom2.js
@@ -1,5 +1,16 @@
-function info(){
-    alert("I am using the event")
-}
-let btn=document.getElementById("btn")
-btn.addEventListener("click",info)
+//! ex 1
+// function info(){
+//     alert("I am using the event")
+// }
+// let btn=document.getElementById("btn")
+// btn.addEventListener("click",info)
+
+//! ex 2
+// document.getElementById("btn").addEventListener("click",()=>{
+//     alert("I am using the event")
+// })
+
+//! ex 3
+document.getElementById("btn").addEventListener("click",function info(){
+    alert("Hii i am an event")
+})
